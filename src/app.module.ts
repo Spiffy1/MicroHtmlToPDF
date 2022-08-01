52f09c98fbb7223fce3b5523e1dfa9e07e6ca2be
SpiffyMammoth <phann8813@gmail.com>
2022-08-01 11:53:45 +0700

    refactor: import path

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { ConfigModule } from '@nestjs/config'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { ConverterModule } from './converter/converter.module'
-import { CoreModule } from './core/core.module'
+import { CoreModule } from './core'
 
 @Module({
   imports: [ConverterModule, CoreModule, ConfigModule.forRoot()],
